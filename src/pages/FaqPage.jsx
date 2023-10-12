eede3ba19650c3b040af9fae540d9cefeeb4e3f5
Yunj0ng <chuccat@gmail.com>
2023-10-12 23:48:29 +0900

    style: modify styles

--- src/pages/FaqPage.jsx
+++ src/pages/FaqPage.jsx
@@ -1,9 +1,17 @@
+import styles from "@styles/page.module.scss"
 import Layout from "@components/Layout/Layout.jsx";
 
 const FaqPage = () => {
-	<>
-    <Layout></Layout>
-  </>;
+  return (
+    <>
+      <Layout>
+        <div className={styles.wrapper}>
+          <h1>FAQ</h1>
+          <div className={styles.faqContent}> </div>
+        </div>
+      </Layout>
+    </>
+  );
 };
 
 export default FaqPage;
